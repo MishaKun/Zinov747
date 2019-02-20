5df6180b8d00e6c343101497de7955d4c922dcd9
Sam <dudoser08@gmail.com>
2019-02-21 02:50:09 +0700

    laba timp take it boy

---
+++ 02_a.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main (void)
+{
+    int q,w,e;
+    scanf("%d%d",&q,&w);
+    e=q+w;
+    printf("%d\n",e);
+    return 0;
+}
---
+++ 03_a.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+int main (void)
+{
+    int q,w,e;
+    scanf("%d%d",&q,&w);
+    e = pow(q,w);
+    printf("%d\n",e);
+return 0;
+}
