ee4d459eb1b0afe5bbb7967a497697fa35f4c47a
MishaKun <47640925+MishaKun@users.noreply.github.com>
2019-03-29 02:27:11 +0700

    Update 03_pow.c

--- 03_pow.c
+++ 03_pow.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 #include <math.h>
-int main (void)
+int main ()
 {
     int q,w,e;
     scanf("%d%d",&q,&w);
-    e = pow(q,w);
-    printf("%d\n",e);
+    printf("%d\n",e = pow(q,w));
 return 0;
 }
