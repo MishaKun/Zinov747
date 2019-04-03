cd4ec9b959a59829e19badca40a8601dcd9908de
MishaKun <47640925+MishaKun@users.noreply.github.com>
2019-04-03 12:55:37 +0700

    Update 01_misha.c

--- 01_misha.c
+++ 01_misha.c
@@ -1,65 +1,74 @@
 #include <stdio.h>
 #include<stdlib.h>
 
-struct node { 
+typedef struct node { //инициализация структуры 
+//в которой будут хранится значения элементов списка и ссылки на них
  	int v;
 	struct node *next;
 } node;
 
-struct list{		
+typedef struct list{//инициализация структуры в которой будет хранится первое значение списка		
 	struct node* first;	
 } list;
 
-int isEmpty(struct list* struc)
-{
-	return struc->first==NULL;
-}
 
-struct node* find(struct list* struc,int value)
+void init( list* struc,int x)//инициализация 
 {
-	struct node *t=struc->first;
-	while(t->v!=value){
-		if(t->next!=NULL)
-			t=t->next;
-		else
-			return 0;
-	}
-	return t;
-}
-
-void init(struct list* struc,int value)
-{
-	struct node* t;
-	t = (struct node*)malloc(sizeof(struct node));
-	t->v=value;
+    node* t = ( node*)malloc(sizeof( node));
+	t->v=x;
 	t->next=NULL;
 	struc->first = t;
 }
+int isEmpty( list* struc)//проверка на пустоту
+{
+	if (struc ->first == NULL)
+	return 1;
+}
+ node* find( list* struc,int x)// поиск определенного значения
+{
+	 node *t_=( node*)malloc(sizeof(node));
+	t_=struc->first;
+	while(t_->v!=x){
+		if(t_->next!=NULL)
+			t_=t_->next;
+		else
+			return 0;
+	}
+	return t_;
+}
 
-int push_back(struct list* struc, int x)
+
+int push_back( list* struc, int x)//вставка назад
 {
 	if(isEmpty(struc)==1)
-		init (struc,x);
+		{
+		    init (struc,x);
+		}
 	else
 	{
-		struct node* t = (struct node*)malloc(sizeof(struct node));
+		node* t = ( node*)malloc(sizeof( node));
 		t->v=x;
 		t->next=NULL;
-		struct node* te = struc->first;
+	    node* te = (node*)malloc(sizeof(node));
+	    te = struc->first;
 		while(te->next!=NULL)
-			te=te->next;
+			
+			    te=te->next;
+			
 		te->next=t;
+			
+		
 	}
 	return 0;
 }
 
-int push_front(struct list* struc,int x) 
+int push_front( list* struc,int x) //вставка вперед
 {
 	if(isEmpty(struc)==1)
 		init (struc,x);
 	else
 	{
-		struct node *t = (struct node*)malloc(sizeof(struct node));
+		node *t = ( node*)malloc(sizeof( node));
 		t->v=x;
 		t->next=struc->first;
 		struc->first=t;
@@ -67,71 +76,55 @@ int push_front(struct list* struc,int x)
 	return 0;
 }
 
-int clear(struct list* struc)
+int removeFirst( list* struc, int x)//удаление первого элемента
 {
-	if(isEmpty(struc)==1)
-	{
-		struct node *t=struc->first;
-		struct node *te=NULL;
-		while(t!=NULL)
-		{
-			te=t->next;
-			free(t);
-			t=te;
-		}
-		struc->first=NULL;
-	}
-}
-
-void _remove(struct list* struc, struct node *t){
-	if(t!=NULL){
-		if (t==struc->first)
-			struc->first=t->next;
+    node* elem = (node *)malloc(sizeof(node));
+    elem = find(struc,x);
+	if (elem!=NULL){
+		
+		if (elem==struc->first)
+			struc->first=elem->next;
 		else
 		{
-			struct node *te = (struct node*)malloc(sizeof(struct node));
+			node *te = ( node*)malloc(sizeof(node));
 			te=struc->first;
-			while(te->next!=t)
+			while(te->next!=elem)
 				te=te->next;
-			te->next = t->next;
+			te->next = elem->next;
 		}
-		free(t);
-	}
-}
-
-int removeFirst(struct list* struc, int x)
-{
-	struct node* element = find(struc,x);
-	if (element!=NULL){
-		_remove(struc,element);
+		free(elem);
+	
 		return 0;
 	}
-	return -1;
+	return 1;
 }
 
-int insertAfter	(struct list* struc,int num, int value)
+int insertAfter	( list* struc,int num, int x)// вставка после определенного элемента
 {
-	struct node *te = malloc(sizeof(node));
-	te->v=value;
+	 node *te = ( node* )malloc(sizeof(node));
+	te->v=x;
 
-	struct node* t = struc->first;
-	struct node* prev_t = struc->first;
+	node* t = ( node* )malloc(sizeof(node)); 
+	t = struc->first;
+	node* r = ( node* )malloc(sizeof(node)); 
+	r = struc->first;
 	
 	for (int i = 1;i<num+1;i++)
 	{
-		prev_t=t;
+		r=t;
 		t=t->next;
 		if(t==NULL) 
 			return -1;
 	}
-	prev_t->next=te;
+	r->next=te;
 	te->next=t;
 	return 0;   
 }
 
-void print(struct list* struc)
+void print( list* struc)// вывод списка
 {
-	struct node* t=struc->first;
+	 node* t=(node *)malloc(sizeof(node));
+	 t=struc->first;
 	while(t->next!=NULL)
 	{
 		printf("%d ", t->v);
@@ -140,37 +133,55 @@ void print(struct list* struc)
 	printf("%d\n", t->v);
 }
 
+
+int clear( list* list_)//очищение списка (Стратхольма)
+{
+	
+		 node * t =(node*)malloc(sizeof(node));
+		 t=list_->first;
+		 node *te=(node*)malloc(sizeof(node));
+		 te=NULL;
+		while(t!=NULL)
+		{
+			te=t->next;
+			free(t);
+			t=te;
+		}
+		list_->first=NULL;
+	
+}
+
 int main()
 {
 	int a,y,k[3],m,t,j,x,z;
-    struct list* list=(struct list*)malloc(sizeof(struct list));	
+    list* list_= (list*)malloc(sizeof( list));	
 	scanf("%d",&a);
 	for (int i=0;i<a;i++)
 	{
 		scanf("%d",&y);
-		push_back(list,y);
+		push_back(list_,y);
 	}
-	print(list);
+	print(list_);
         scanf("%d%d%d",&k[0],&k[1],&k[2]);
 	for(int i=0;i<3;i++){
-		if(find(list,k[i])!=NULL)
+		if(find(list_,k[i])!=NULL)
 			printf("1 ");
 		else
 			printf("0 ");
 	}
 	printf("\n");
 	scanf("%d",&m);
-	push_back(list,m);
-	print(list);
+	push_back(list_,m);
+	print(list_);
 	scanf("%d",&t);
-	push_front(list,t);
-	print(list);
+	push_front(list_,t);
+	print(list_);
 	scanf("%d%d",&j,&x);
-	insertAfter(list,j,x);
-	print(list);
+	insertAfter(list_,j,x);
+	print(list_);
 	scanf("%d",&z);
-	removeFirst(list,z);
-	print(list);
-	clear(list);
+	removeFirst(list_,z);
+	print(list_);
+	clear(list_);
 	return 0;
 }
