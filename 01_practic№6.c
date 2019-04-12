857f9b1f1d5076d689ac62a1cc16f6d65ae6f381
MishaKun <47640925+MishaKun@users.noreply.github.com>
2019-04-12 13:55:03 +0700

    Update 01_practic№6.c

--- 01_practic№6.c
+++ 01_practic№6.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 void sorting_function(int n, int *mass){
-        int y = n-1;
-        int tmp;
-        while(y>=1){
-                for(int i=0;i<n-y;i++){
-                        if(mass[i]>mass[i+y]){
-                                tmp=mass[i];
-                                mass[i]=mass[i+y];
-                                mass[i+y]=tmp;
-                        }
-                }
-                y = y - 1 ;
+  int y = n-1;
+  int tmp;
+  while(y>=1)
+  {
+    for(int i=0;i<n-y;i++){
+    if(mass[i]>mass[i+y]){
+    tmp=mass[i];
+    mass[i]=mass[i+y];
+    mass[i+y]=tmp;
+      }
+    }
+   y = y - 1 ;
         }
 }
 int main ()
